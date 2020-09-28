b2bf77387ff6549661b09ced59917625ce85a00d
longyyu <longyyu@umich.edu>
2020-09-29 06:07:30 +0800

    Modifying header and deleting unnecessary comments

--- activities/week1/cutnames.sh
+++ activities/week1/cutnames.sh
@@ -1,37 +1,23 @@
 #!/bin/env bash
-# Stats 506, Fall 2020
-# Updated: Sep 10, 2020 by Yanyu Long
+
+# Column Selector
+# Takes two arguments, `file` and `pattern`, 
+#   and return the extracted columns
+# Week 1 Activity - Stats 506, Fall 2020
+# Author: Yanyu Long, longyyu@umich.edu
+# Updated: Sep 29, 2020
+
 # 79: -------------------------------------------------------------------------
 file=$1
 pattern=$2
 
-# a - download data if not present
-# file="recs2015_public_v4.csv" 
-# url="https://www.eia.gov/consumption/residential/data/2015/csv/recs2015_public_v4.csv"
-
-## if the file doesn't exist
-# if [ ! -f "$file" ]; then
-#   wget $url
-# fi
-
-# b - extract header row and output to a file with one name per line
-# new_file="recs_names.txt"
-
-## delete new_file if it is already present
-# if [ -f "$new_file" ]; then
-#   rm "$new_file"
-# fi
-
-# <$file head -n1 | tr , \\n > $new_file
-
+# extracts the column number of variables specified by `pattern`
+#   and paste them with "," into a single string `cols`
 cols=$(
-  <$file head -n1 | tr , \\n | grep -nE $pattern | cut -d: -f1 | tr \\n ,  
-# <$new_file grep -nE $pattern | cut -d: -f1 | tr \\n ,    
+  <$file head -n1 | tr , \\n | grep -nE $pattern | cut -d: -f1 | paste -d, -s 
 )
-cols=${cols%?} # delete the last comma
-# echo $cols
 
 # d - cut out the appropriate columns and save as recs_brrweights.csv
-cut -d, -f$cols $file # > recs_brrweights.csv
+cut -d, -f$cols $file
 
 # 79: -------------------------------------------------------------------------
