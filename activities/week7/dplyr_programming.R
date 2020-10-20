578be031346d8f5fbf3fcd64d0a4c467e777f8b8
longyyu <longyyu@umich.edu>
2020-10-20 23:30:12 +0800

    Finishing all tasks of week 7 activity

--- activities/week7/dplyr_programming.R
+++ activities/week7/dplyr_programming.R
@@ -1,4 +1,6 @@
 # Functional programming with dplyr
+# Week 7 activity
+# Stats506, F20
 # 
 # In this script, we write functions that use dplyr to
 # compute weighted means.
@@ -7,8 +9,9 @@
 # https://www.eia.gov/consumption/residential/data/2015/index.php
 #   ?view=microdata
 #
+# Author: Rithu Uppalapati, Yawen Hu, 
+#         Yanyu Long, Chen Shang
 # Updated: October 20, 2020
-# Author: James Henderson
 # 79: -------------------------------------------------------------------------
  
 # run tests at the end?: ------------------------------------------------------
@@ -41,7 +44,11 @@ recs_core =
     weight = NWEIGHT,
     
     # grouping factor
-    region = factor(REGIONC, c('Northeast', 'Midwest', 'South', 'West')),
+    region = factor(
+      REGIONC, 
+      levels = 1:4, 
+      labels = c('Northeast', 'Midwest', 'South', 'West')
+    ),
     
     # case selection
     heat_home = factor(HEATHOME, 0:1, c('No', 'Yes') ),
@@ -56,8 +63,8 @@ recs_core =
 
 # filter cases to those that use space heating in winter: ---------------------
 
-## shows why we want to do this, temps are missing if space heating not used
-#recs_core %>% 
+# # shows why we want to do this, temps are missing if space heating not used
+# recs_core %>%
 #  filter(heat_home == 'No') %>%
 #  summarize_all( .funs = function(x) sum(is.na(x)) )
 
@@ -76,12 +83,16 @@ temps_by_region0 =
     .groups = 'drop'
   )
 
-## task 1 - replace the repetition above using `across()`
-#temps_by_region1 = recs_core %>% 
-#  group_by(region) %>%
-#  summarize(
-#    <task 1>
-#  )
+# task 1 - replace the repetition above using `across()`
+temps_by_region1 = recs_core %>%
+ group_by(region) %>%
+ summarize(
+   across(
+     starts_with("temp"), 
+     ~weighted.mean(.x, weight, na.rm = TRUE)
+   ),
+   .groups = "drop"
+ )
 
 ## task 2 - write a function using the pattern above
 recs_mean0 = function(df, vars) {
@@ -92,75 +103,80 @@ recs_mean0 = function(df, vars) {
   #
   # Outputs: a tibble with one row (per group) as returned by summarize_at
 
-  # <task2>
+  df  %>%
+    summarize(across(.cols = all_of(vars), 
+              ~weighted.mean(.x, weight, na.rm = TRUE))
+  )
 }
 
-
 # Don't be afraid to do some of the work outside the function
-#temps_by_region = recs_core %>% 
+# temps_by_region = recs_core %>%
 #  group_by(region) %>%
 #  recs_mean0( vars = c('temp_home', 'temp_gone', 'temp_night') )
 
 ## task 3: write a function `add_groups()` to group a data frame
-add_groups = function(df, group = NULL) {
+add_groups = function(df, groups = NULL) {
   # adds grouping variables to a data.frame and/or tibble
   # Inputs:
   #   df - an object inheriting from the data.frame class, commonly a tibble
-  #   group - (optional, defaults to NULL) a character vector of column
+  #   groups - (optional, defaults to NULL) a character vector of column
   #    names in df to form groups by.
-
-  # <task 3> 
   
-  df
+  if (!is.null(groups)){
+    stopifnot(all(groups %in% names(df)))
+    for (group in groups){
+      df = df %>%
+        group_by(.data[[group]], .add = TRUE)
+    }
+  }
+  return(df)
 }
 
 
 ## task 4: write a functional version with groups
-recs_mean1 = function(df, vars, group = NULL) {
+recs_mean1 = function(df, vars, groups = NULL) {
   # Inputs
   #  df: a (possibly grouped) tibble or data.frame object to be summarized
   #      df must have a variable 'weight' for the weighted sums. 
   #  vars: a character vector of numeric variables in 
-  #  group: a character vector with variable names to group by. If 
+  #  groups: a character vector with variable names to group by. If 
   #         NULL (the default) retains an group structure of `df` as passed.
   #
   # Outputs: a tibble with one row (per group) as returned by summarize_at
   
-  # call `add_groups()`
-  #<task 4a>
-  
-  # copy your summarize code from task 2 / `recs_mean0()`
-  #<task 4b>
-  
+  df %>%
+    add_groups(groups = groups) %>%
+    summarize(
+      across(
+        .cols = all_of(vars), 
+        ~weighted.mean(.x, weight, na.rm = TRUE)
+      ),
+      .groups = "drop"
+    )
 }
 
 ## Example uses: 
 if ( TEST ) {
+  recs_mean1(recs_core, vars = c('temp_home', 'temp_gone', 'temp_night') )
 
- recs_mean1(recs_core, vars = c('temp_home', 'temp_gone', 'temp_night') )
-
- recs_core %>%
-   group_by(region) %>%
-   recs_mean1( vars = c('temp_home', 'temp_gone', 'temp_night'))
+  recs_core %>%
+    group_by(region) %>%
+    recs_mean1( vars = c('temp_home', 'temp_gone', 'temp_night'))
 
   recs_mean1(recs_core, vars = c('temp_home', 'temp_gone', 'temp_night'),
            group = 'region')
 
-
  ## pivot to a longer format
- df = 
-  recs_core %>%
-  select(id, weight, region, starts_with('temp_') ) %>%
-  pivot_longer( 
-    cols = starts_with('temp'),
-    names_to = 'type',
-    names_prefix = 'temp_',
-    values_to = 'temp'
+  df = recs_core %>%
+    select(id, weight, region, starts_with('temp_') ) %>%
+    pivot_longer( 
+      cols = starts_with('temp'),
+      names_to = 'type',
+      names_prefix = 'temp_',
+      values_to = 'temp'
   )
 
- temps_by_type_region =
-  df %>%
-  #group_by(type, region) %>% recs_mean1( vars = 'temp' )
-  recs_mean1( vars = c('temp'), group = c('type', 'region'))
+  temps_by_type_region = df %>%
+    #group_by(type, region) %>% recs_mean1( vars = 'temp' )
+    recs_mean1( vars = c('temp'), group = c('type', 'region'))
 }
-
